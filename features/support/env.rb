487ec65ffe97ac5f13305203ced8cd2393ae791a
gmoraya <gabrielmoraya@gmail.com>
2023-12-10 22:38:51 -0300

    teste

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,8 @@ require 'selenium-webdriver'
 require 'rspec/expectations'
 
 
+
+
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome # ou :selenium para o navegador padrão
   config.app_host = 'https://pauliceia.unifesp.br/portal/home' 
